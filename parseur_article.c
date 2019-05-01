4b2910101fc5d2492fbaa2f0df7754a4e191a61e
Yrrieth <thierry.wen@laposte.net>
2019-05-01 20:24:27 +0200

    Ajout fonction compare_pron_suj()

--- parseur_article.c
+++ parseur_article.c
@@ -2,10 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-int compare (char buffer[]) {
+/**
+ * Enlève espace et ponctuation du paramètre d'entrée
+ */
+char* clarifie_string (char buffer[]) {
+	int i;
+	char* resultat = malloc(256 * sizeof(char));
+	int indexResultat = 0;
+	for (i = 0; i < strlen(buffer); i++) {
+		// Between A-Z || a-z
+		if ((buffer[i] >= 65 && buffer[i] <= 90) || (buffer[i] >= 97 && buffer[i] <= 122)) {
+			resultat[indexResultat++] = buffer[i];
+		}
+	}
+	return resultat;
+}
+
+int compare_mot (char buffer[]) {
 	int i, j;
-	char *mot[] = {"le", "Le", "la", "La", "les", "Les", "l", "L"};
 	int identique = 0;
+	char *mot[] = {"le", "Le", "la", "La", "les", "Les", "l", "L"};
+	int nombreMaxMot = sizeof(mot)/sizeof(mot[0]);
+
+	//buffer = clarifie_string(buffer);
+	//printf("%s\n", buffer);
+
+	for (i = 0; i < nombreMaxMot; i++) {
+		if (strcmp(buffer, mot[i]) == 0) {
+			identique = 1;
+			return identique;
+		}
+	}
+	return identique;
+}
+
+int compare_pron_suj (char buffer[]) {
+	int i, j;
+	int identique = 0;
+	char *mot[] = {"je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles",
+				   "Je", "Tu", "Il", "Elle", "On", "Nous", "Vous", "Ils", "Elles",
+				   "me", "te", "se"};
 	int nombreMaxMot = sizeof(mot)/sizeof(mot[0]);
 	for (i = 0; i < nombreMaxMot; i++) {
 		if (strcmp(buffer, mot[i]) == 0) {
@@ -16,33 +52,128 @@ int compare (char buffer[]) {
 	return identique;
 }
 
+/*
 void lire (FILE* fichier_entree, FILE* fichier_a){
-	int c = 0,c2 = 0, c3 = 0;
+	int c = 0;
 	int taille = 256;
-	int indexCourant = 0;
+	int indexCourant = 0; // index courant du buffer
 	char buffer[taille];
 	int identique;
-
-	while((c = fgetc(fichier_entree)) != EOF) { // Boucle sur tous le texte
-		if (c == '.' || c == '!' || c == '?' || c == ' ' || (c == '\'')) {
+	char motPrecedent[taille];
+	char apostrophe[3];
+	int pron_suj;
+	char balise_ouv[taille];
+	char balise_ferm[taille];
+	
+	while((c = fgetc(fichier_entree)) != EOF) { // Boucle sur chaque caractère du texte
+		if (c == '.' || c == '!' || c == '?' || c == ' ' || (c == '\'')) { // Si c'est une ponctuation == Fin de mot
 			indexCourant = 0;
-			identique = compare(buffer);
+			identique = compare_mot
+
+			(buffer);
+
+			//printf("%s\n", motPrecedent);
 
 			if (identique == 1) {
-				fprintf(fichier_a, "<trouve>");
+				//pron_suj = compare_pron_suj(motPrecedent);
+				//if ()
+				strcpy(balise_ouv, "<trouve>");
+				strcpy(balise_ferm, "</trouve>");
+
+				fprintf(fichier_a, "%s", balise_ouv);
 
 				if (c == '\'') { // Dans le cas où c vaut l' ou L'
 					fprintf(fichier_a, "%s%c", buffer, c);
-					fprintf(fichier_a, "</trouve>");
+					fprintf(fichier_a, "%s", balise_ferm);
+					sprintf(apostrophe, "%d", c);  // int devient un char*
+					strcat(buffer, apostrophe);    // Concatène (l ou L) et '
 				} else {
 					fprintf(fichier_a, "%s", buffer);
-					fprintf(fichier_a, "</trouve>%c", c);
+					fprintf(fichier_a, "%s%c", balise_ferm, c);
 				}
-				memset(buffer, 0, sizeof(buffer)); // Pour effacer le contenu du buffer
 			} else {
 				fprintf(fichier_a, "%s%c", buffer, c);
-				memset(buffer, 0, sizeof(buffer)); // Pour effacer le contenu du buffer
 			}
+			memset(motPrecedent, 0, sizeof(motPrecedent)); // Pour effacer le contenu de motPrecedent
+			
+			// strcpy(motPrecedent, buffer);
+			// Copie le buffer actuel
+			// pour que dans la boucle suivante,
+			// la copie devient le mot précédent
+			//
+			strcpy(motPrecedent, buffer);
+
+			memset(buffer, 0, sizeof(buffer)); // Pour effacer le contenu du buffer
+		} else { 
+			buffer[indexCourant] = c;
+			indexCourant++;
+		}
+	}
+}
+*/
+
+void lire (FILE* fichier_entree, FILE* fichier_a){
+	int c = 0;
+	int taille = 256;
+	int indexCourant = 0; // index courant du buffer
+	char buffer[taille];
+	char motPrecedent[taille];
+	char apostrophe[3];
+
+	// boolean
+	int identique;
+	int pron_suj;
+	
+	
+	
+	while((c = fgetc(fichier_entree)) != EOF) { // Boucle sur chaque caractère du texte
+		if (c == ',' || c == '.' || c == '!' || c == '?' || c == ' ' || (c == '\'')) { // Si c'est une ponctuation == Fin de mot
+			indexCourant = 0;
+			identique = compare_mot(buffer);
+
+			//printf("%s\n", motPrecedent);
+
+			if (identique == 1) {
+				pron_suj = compare_pron_suj(motPrecedent);
+				if (pron_suj == 1) {
+					fprintf(fichier_a, "<pronom>");
+				} else {
+					fprintf(fichier_a, "<trouve>");
+				}
+
+				if (c == '\'') { // Dans le cas où c vaut l' ou L'
+					fprintf(fichier_a, "%s%c", buffer, c);
+
+					if (pron_suj == 1) {
+						fprintf(fichier_a, "</pronom>");
+					} else {
+						fprintf(fichier_a, "</trouve>");
+					}
+
+					sprintf(apostrophe, "%d", c);  // int devient un char*
+					strcat(buffer, apostrophe);    // Concatène (l ou L) et '
+				} else {
+					fprintf(fichier_a, "%s", buffer);
+
+					if (pron_suj == 1) {
+						fprintf(fichier_a, "</pronom>%c", c);
+					} else {
+						fprintf(fichier_a, "</trouve>%c", c);
+					}
+				}
+			} else {
+				fprintf(fichier_a, "%s%c", buffer, c);	
+			}
+			memset(motPrecedent, 0, sizeof(motPrecedent)); // Pour effacer le contenu de motPrecedent
+			
+			/** strcpy(motPrecedent, buffer);
+			 * Copie le buffer actuel
+			 * pour que dans la boucle suivante,
+			 * la copie devient le mot précédent
+			 */
+			strcpy(motPrecedent, buffer);
+
+			memset(buffer, 0, sizeof(buffer)); // Pour effacer le contenu du buffer
 		} else {
 			buffer[indexCourant] = c;
 			indexCourant++;
@@ -56,17 +187,17 @@ int main (int argc, char *argv[]){
 		exit(1);
 	}
 	FILE* fichier_entree = NULL;
-    FILE* fichier_sortie = NULL;
+	FILE* fichier_sortie = NULL;
 
 	fichier_entree = fopen(argv[1], "r+");
-    fichier_sortie = fopen(argv[2], "w+");
+	fichier_sortie = fopen(argv[2], "w+");
 	if(fichier_entree == NULL || fichier_sortie == NULL){
 		fprintf(stderr, "bug à la lecture/création du fichier\n");
 		exit(1);
 	}
 
 	fprintf(fichier_sortie, "<?xml version=\"1.0\" encoding=\"utf-8\">\n");
-    lire(fichier_entree,fichier_sortie);
+	lire(fichier_entree, fichier_sortie);
 	fclose(fichier_entree);
 	fclose(fichier_sortie);
 	return 0;
