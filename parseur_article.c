483a5eb3fe39536165aa10c6221aec33ad4a6de9
Yrrieth <thierry.wen@laposte.net>
2019-05-02 20:14:02 +0200

    Petite correction partout

--- parseur_article.c
+++ parseur_article.c
@@ -22,7 +22,7 @@ int compare_pron_suj (char buffer[]) {
 	int identique = 0;
 	char *mot[] = {"je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles",
 				   "Je", "Tu", "Il", "Elle", "On", "Nous", "Vous", "Ils", "Elles",
-				   "me", "te", "se"};
+				   "me", "te", "se", "J", "j"};
 	int nombreMaxMot = sizeof(mot)/sizeof(mot[0]);
 	for (i = 0; i < nombreMaxMot; i++) {
 		if (strcmp(buffer, mot[i]) == 0) {
@@ -53,7 +53,7 @@ int compare_prepo_articl (char buffer[]) {
 	int identique = 0;
 	char *mot[] = {"sur", "sous", "entre", "devant", "derrière", "dans", "chez",
 				   "avant", "après", "vers", "depuis", "pendant", "pour",
-				   "vers"};
+				   "vers", "tout", "toute", "tous", "toutes"};
 	int nombreMaxMot = sizeof(mot)/sizeof(mot[0]);
 	for (i = 0; i < nombreMaxMot; i++) {
 		if (strcmp(buffer, mot[i]) == 0) {
@@ -64,86 +64,31 @@ int compare_prepo_articl (char buffer[]) {
 	return identique;
 }
 
-/*
 void lire (FILE* fichier_entree, FILE* fichier_a){
+	// index
 	int c = 0;
 	int taille = 256;
 	int indexCourant = 0; // index courant du buffer
-	char buffer[taille];
-	int identique;
-	char motPrecedent[taille];
-	char apostrophe[3];
-	int pron_suj;
-	char balise_ouv[taille];
-	char balise_ferm[taille];
-	
-	while((c = fgetc(fichier_entree)) != EOF) { // Boucle sur chaque caractère du texte
-		if (c == '.' || c == '!' || c == '?' || c == ' ' || (c == '\'')) { // Si c'est une ponctuation == Fin de mot
-			indexCourant = 0;
-			identique = compare_mot
 
-			(buffer);
-
-			//printf("%s\n", motPrecedent);
-
-			if (identique == 1) {
-				//pron_suj = compare_pron_suj(motPrecedent);
-				//if ()
-				strcpy(balise_ouv, "<trouve>");
-				strcpy(balise_ferm, "</trouve>");
-
-				fprintf(fichier_a, "%s", balise_ouv);
-
-				if (c == '\'') { // Dans le cas où c vaut l' ou L'
-					fprintf(fichier_a, "%s%c", buffer, c);
-					fprintf(fichier_a, "%s", balise_ferm);
-					sprintf(apostrophe, "%d", c);  // int devient un char*
-					strcat(buffer, apostrophe);    // Concatène (l ou L) et '
-				} else {
-					fprintf(fichier_a, "%s", buffer);
-					fprintf(fichier_a, "%s%c", balise_ferm, c);
-				}
-			} else {
-				fprintf(fichier_a, "%s%c", buffer, c);
-			}
-			memset(motPrecedent, 0, sizeof(motPrecedent)); // Pour effacer le contenu de motPrecedent
-			
-			// strcpy(motPrecedent, buffer);
-			// Copie le buffer actuel
-			// pour que dans la boucle suivante,
-			// la copie devient le mot précédent
-			//
-			strcpy(motPrecedent, buffer);
-
-			memset(buffer, 0, sizeof(buffer)); // Pour effacer le contenu du buffer
-		} else { 
-			buffer[indexCourant] = c;
-			indexCourant++;
-		}
-	}
-}
-*/
-
-void lire (FILE* fichier_entree, FILE* fichier_a){
-	int c = 0;
-	int taille = 256;
-	int indexCourant = 0; // index courant du buffer
-	char buffer[taille];
-	char motPrecedent[taille];
-	char apostrophe[3];
+	// string
+	char buffer[taille];       // Stocke le mot actuel
+	char motPrecedent[taille]; // Stocke le mot précédent le buffer
+	char apostrophe[2];        // Stocke le caractère ' 
+                               //(ici, fgetc retourne un entier qu'il faudra convertir en char grâce à sprintf() pour concaténer avec strcat(), voir plus bas)
 
 	// boolean
-	int identique;
+	int mot_identique;
 	int pron_suj;
 	int prepo_articl;
 	
 	while((c = fgetc(fichier_entree)) != EOF) { // Boucle sur chaque caractère du texte
 		if (c == ',' || c == '.' || c == '!' || c == '?' || c == ' ' || c == '\'' ||
-			c == '\t' || c == '\n') { // Si c'est une ponctuation == Fin de mot
+			c == ';' || c == '-' || c == '\t' || c == '\n') { // Si c'est une ponctuation == Fin de mot
 			indexCourant = 0;
-			identique = compare_mot(buffer);
+			mot_identique = compare_mot(buffer);
+			//printf("%s\n", buffer);
 
-			if (identique == 1) {
+			if (mot_identique == 1) {
 				pron_suj = compare_pron_suj(motPrecedent);
 				prepo_articl = compare_prepo_articl(motPrecedent);
 
@@ -206,10 +151,15 @@ int main (int argc, char *argv[]){
 	}
 	FILE* fichier_entree = NULL;
 	FILE* fichier_sortie = NULL;
+	FILE* fichier_article = NULL;
+	FILE* fichier_pronom = NULL;
 
 	fichier_entree = fopen(argv[1], "r+");
 	fichier_sortie = fopen(argv[2], "w+");
-	if(fichier_entree == NULL || fichier_sortie == NULL){
+	fichier_article = fopen("fichier_article.txt", "a+");
+	fichier_pronom = fopen("fichier_pronom.txt", "a+");
+
+	if(fichier_entree == NULL || fichier_sortie == NULL || fichier_article == NULL || fichier_pronom == NULL){
 		fprintf(stderr, "bug à la lecture/création du fichier\n");
 		exit(1);
 	}
