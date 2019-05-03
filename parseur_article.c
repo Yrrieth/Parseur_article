70dbf965b2bc7f36e29fdf0bb32421ca3a2d6527
Yrrieth <thierry.wen@laposte.net>
2019-05-03 20:00:42 +0200

    Ajout 2ème lecture du texte à parser

--- parseur_article.c
+++ parseur_article.c
@@ -64,25 +64,39 @@ int compare_prepo_articl (char buffer[]) {
 	return identique;
 }
 
-void parse_fichier (FILE* fichier_dico, char buffer[]) {
+int lit_fichier_dico (FILE* fichier_dico, char buffer[]) {
 	int c;
 	int taille = 256;
 	char mot_fichier[taille];
 	while (fgets(mot_fichier, taille, fichier_dico) != NULL) {
 		mot_fichier[strlen(mot_fichier) - 1] = '\0';
 		if (strcmp(mot_fichier, buffer) == 0) {
-			//printf("%sICI\n", mot_fichier);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void cree_fichier_dico (FILE* fichier_dico, char buffer[]) {
+	int c;
+	int taille = 256;
+	char mot_fichier[taille];
+	while (fgets(mot_fichier, taille, fichier_dico) != NULL) {
+		mot_fichier[strlen(mot_fichier) - 1] = '\0';
+		if (strcmp(mot_fichier, buffer) == 0) {
 			return;
 		}
 	}
 	fprintf(fichier_dico, "%s\n", buffer);
 }
 
-void lire (FILE* fichier_entree, FILE* fichier_a, FILE* fichier_article, FILE* fichier_pronom){
+void lire (FILE* fichier_entree, FILE* fichier_a, FILE* fichier_article, FILE* fichier_pronom, int prem_lecture){
 	// index
 	int c = 0;
 	int taille = 256;
 	int indexCourant = 0; // index courant du buffer
+	int ponctuationPrecedente;
+
 
 	// string
 	char buffer[taille];       // Stocke le mot actuel
@@ -96,8 +110,14 @@ void lire (FILE* fichier_entree, FILE* fichier_a, FILE* fichier_article, FILE* f
 	int prepo_articl;
 	int bool_motSuiv;  // Initialisé lorsqu'on trouve un mot identique,
 	                   // Puis, lors de la boucle suivante, stocke le buffer dans un fichier
-	
+	int bool_motPrecedent;
+
+	if (prem_lecture == 0) {
+		fseek(fichier_entree, 0, SEEK_SET);
+	}
+
 	while((c = fgetc(fichier_entree)) != EOF) { // Boucle sur chaque caractère du texte
+
 		if (c == ',' || c == '.' || c == '!' || c == '?' || c == ' ' || c == '\'' ||
 			c == ';' || c == '-' || c == '\t' || c == '\n') { // Si c'est une ponctuation == Fin de mot
 			indexCourant = 0;
@@ -111,61 +131,98 @@ void lire (FILE* fichier_entree, FILE* fichier_a, FILE* fichier_article, FILE* f
 				if (pron_suj == 1) {
 					fprintf(fichier_a, "<pronom>");
 					bool_motSuiv = 1;
-					
+					bool_motPrecedent = 0;
+
 					fichier_pronom = fopen("fichier_pronom.txt", "a+");
 				} else if (prepo_articl == 1) {
 					fprintf(fichier_a, "<article>");
 					bool_motSuiv = 2;
+					bool_motPrecedent = 0;
 
 					fichier_article = fopen("fichier_article.txt", "a+");
 				} else {
-					fprintf(fichier_a, "<trouve>");
+					if (prem_lecture == 1) {
+						fprintf(fichier_a, "<trouve>");
+					} else if (prem_lecture == 0) {
+						bool_motPrecedent = 1;
+					}
 				}
 
-				if (c == '\'') { // Dans le cas où c vaut l' ou L'
-					fprintf(fichier_a, "%s%c", buffer, c);
+				if (prem_lecture == 1) {
+					if (c == '\'') { // Dans le cas où c vaut l' ou L'
+						fprintf(fichier_a, "%s%c", buffer, c);
 
-					if (pron_suj == 1) {
-						fprintf(fichier_a, "</pronom>");
-					} else if (prepo_articl == 1) {
-						fprintf(fichier_a, "</article>");
+						if (pron_suj == 1) {
+							fprintf(fichier_a, "</pronom>");
+						} else if (prepo_articl == 1) {
+							fprintf(fichier_a, "</article>");
+						} else {
+							fprintf(fichier_a, "</trouve>");
+						}
+
+						sprintf(apostrophe, "%d", c);  // int devient un char*
+						strcat(buffer, apostrophe);    // Concatène (l ou L) et '
 					} else {
-						fprintf(fichier_a, "</trouve>");
-					}
+						fprintf(fichier_a, "%s", buffer);
 
-					sprintf(apostrophe, "%d", c);  // int devient un char*
-					strcat(buffer, apostrophe);    // Concatène (l ou L) et '
-				} else {
-					fprintf(fichier_a, "%s", buffer);
-
-					if (pron_suj == 1) {
-						fprintf(fichier_a, "</pronom>%c", c);
-					} else if (prepo_articl == 1) {
-						fprintf(fichier_a, "</article>%c", c);
-					} else {
-						fprintf(fichier_a, "</trouve>%c", c);
+						if (pron_suj == 1) {
+							fprintf(fichier_a, "</pronom>%c", c);
+						} else if (prepo_articl == 1) {
+							fprintf(fichier_a, "</article>%c", c);
+						} else {
+							fprintf(fichier_a, "</trouve>%c", c);
+						}
 					}
 				}
 			} else { // Sinon, ce n'est pas un mot identique
-				fprintf(fichier_a, "%s%c", buffer, c);
 
-				// On stocke le mot qui suit le mot recherché dans un fichier
-				if (bool_motSuiv == 1) { // Si c'était un pronom
-					//fprintf(fichier_pronom, "%s\n", buffer);
-					parse_fichier(fichier_pronom, buffer);
-					bool_motSuiv = 0;
+				if (prem_lecture == 1) {
+					fprintf(fichier_a, "%s%c", buffer, c);
 
-					fclose(fichier_pronom);
-				} else if (bool_motSuiv == 2) { // Si c'était un article
-					//fprintf(fichier_article, "%s\n", buffer);
-					parse_fichier(fichier_article, buffer);
-					bool_motSuiv = 0;
+					// On stocke le mot qui suit le mot recherché dans un fichier
+					if (bool_motSuiv == 1) { // Si c'était un pronom
+						//fprintf(fichier_pronom, "%s\n", buffer);
+						cree_fichier_dico(fichier_pronom, buffer);
+						bool_motSuiv = 0;
 
-					fclose(fichier_article);
+						fclose(fichier_pronom);
+					} else if (bool_motSuiv == 2) { // Si c'était un article
+						//fprintf(fichier_article, "%s\n", buffer);
+						cree_fichier_dico(fichier_article, buffer);
+						bool_motSuiv = 0;
+
+						fclose(fichier_article);
+					}
+				}  else if (prem_lecture == 0) {
+					//printf("%s\n", motPrecedent);
+					if (compare_mot(motPrecedent) == 1 && bool_motPrecedent == 1) {
+						if (&ponctuationPrecedente == NULL) {
+							ponctuationPrecedente = ' ';
+							printf("BLABLA\n");
+						}
+						
+						fichier_pronom = fopen("fichier_pronom.txt", "a+");
+						fichier_article = fopen("fichier_article.txt", "a+");
+
+						if (lit_fichier_dico(fichier_pronom, buffer) == 1) {
+							fprintf(fichier_a, "<pronom>%s</pronom>%c", motPrecedent, ponctuationPrecedente);
+							fprintf(fichier_a, "%s%c", buffer, c);
+						} else if (lit_fichier_dico(fichier_article, buffer) == 1) {
+							fprintf(fichier_a, "<article>%s</article>%c", motPrecedent, ponctuationPrecedente);
+							fprintf(fichier_a, "%s%c", buffer, c);
+						} else {
+							fprintf(fichier_a, "<trouve>%s</trouve>%c", motPrecedent, ponctuationPrecedente);
+							fprintf(fichier_a, "%s%c", buffer, c);
+						}
+						bool_motPrecedent = 0;
+					} else {
+						fprintf(fichier_a, "%s%c", buffer, c);
+					}
 				}
-				
-				
 			}
+
+			ponctuationPrecedente = c;
+
 			memset(motPrecedent, 0, sizeof(motPrecedent)); // Pour effacer le contenu de motPrecedent
 			
 			/** strcpy(motPrecedent, buffer);
@@ -176,11 +233,22 @@ void lire (FILE* fichier_entree, FILE* fichier_a, FILE* fichier_article, FILE* f
 			strcpy(motPrecedent, buffer);
 
 			memset(buffer, 0, sizeof(buffer)); // Pour effacer le contenu du buffer
-		} else {
+		} else { // Sinon, ce n'est pas une ponctuation
 			buffer[indexCourant] = c;
 			indexCourant++;
 		}
 	}
+}
+
+void cree_fichier_xml (FILE* fichier_entree, FILE* fichier_sortie, FILE* fichier_article, FILE* fichier_pronom, int prem_lecture) {
+	fprintf(fichier_sortie, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+	fprintf(fichier_sortie, "<?xml-stylesheet href=\"res.xsl\" type=\"text/xsl\"?>\n");
+	fprintf(fichier_sortie, "<texte>\n");
+
+	lire(fichier_entree, fichier_sortie, fichier_article, fichier_pronom, prem_lecture);
+
+	fprintf(fichier_sortie, "\n</texte>");
+	fprintf(fichier_sortie, "\n<!-- %d -->\n", prem_lecture);
 }
 
 int main (int argc, char *argv[]){
@@ -203,13 +271,24 @@ int main (int argc, char *argv[]){
 		exit(1);
 	}
 
-	fprintf(fichier_sortie, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
-	fprintf(fichier_sortie, "<?xml-stylesheet href=\"res.xsl\" type=\"text/xsl\"?>\n");
-	fprintf(fichier_sortie, "<texte>\n");
+	int prem_lecture = 1;
 
-	lire(fichier_entree, fichier_sortie, fichier_article, fichier_pronom);
+	cree_fichier_xml(fichier_entree, fichier_sortie, fichier_article, fichier_pronom, prem_lecture);
+	//fseek(fichier_sortie, 0, SEEK_SET);
+	fclose(fichier_sortie);
 
-	fprintf(fichier_sortie, "\n</texte>");
+	int rm = remove(argv[2]);
+	if (rm == 0) {
+		printf("Supprimé avec succès\n");
+	} else {
+		fprintf(stderr, "bug à la suppression du fichier\n");
+		exit(1);
+	}
+
+	fichier_sortie = fopen(argv[2], "w+");
+	prem_lecture = 0;
+	cree_fichier_xml(fichier_entree, fichier_sortie, fichier_article, fichier_pronom, prem_lecture);
+
 	fclose(fichier_entree);
 	fclose(fichier_sortie);
 	//fclose(fichier_pronom);
